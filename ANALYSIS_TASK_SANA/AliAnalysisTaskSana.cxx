e6431def98adb2e6d5e2385c4afbc072b4d3378f
alcaliva <32872606+alcaliva@users.noreply.github.com>
2019-07-01 18:04:27 +0200

    Add files via upload

---
+++ ANALYSIS_TASK_SANA/AliAnalysisTaskSana.cxx
@@ -0,0 +1,287 @@
+#include "AliAnalysisTaskSana.h"
+#include "AliInputEventHandler.h"
+#include "AliAnalysisManager.h"
+#include "AliAnalysisTaskSE.h"
+#include "AliAnalysisUtils.h"
+#include "AliMultSelection.h"
+#include "AliMultEstimator.h"
+#include "AliAnalysisTask.h"
+#include "AliPIDResponse.h"
+#include "TLorentzVector.h"
+#include "AliCentrality.h"
+#include "AliEventplane.h"
+#include "AliEventCuts.h"
+#include "TDatabasePDG.h"
+#include "AliAODVertex.h"
+#include "AliAODTrack.h"
+#include "AliAODEvent.h"
+#include "TObjArray.h"
+#include "TVector2.h"
+#include "TVector3.h"
+#include "TVectorD.h"
+#include "AliAODv0.h"
+#include "TRandom.h"
+#include "TChain.h"
+#include "TMath.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TF1.h"
+
+ClassImp(AliAnalysisTaskSana)
+
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+AliAnalysisTaskSana::AliAnalysisTaskSana():
+AliAnalysisTaskSE(),
+fAODevent(NULL),
+fPIDResponse(NULL),
+fAODeventCuts(),
+fUtils(NULL),
+fOutputList(NULL),
+fQAList(NULL),
+fCentralityMin(0),
+fCentralityMax(0),
+fVertexZmin(0),
+fVertexZmax(0),
+fNumberOfVertexContributorsMin(0),
+fMagFieldSign(0),
+fDCAxyMax(0),
+fDCAzMax(0),
+fPtMin(0),
+fPtMax(0),
+fEtaMax(0),
+fNumberOfClustersITSMin(0),
+fNumberOfClustersTPCMin(0),
+fNumberOfClustersTRDMin(0),
+fNumberOfClustersTPCdEdxMin(0),
+fNumberOfCrossedRowsMin(0),
+fCrossedRowsOverFindableClsMin(0),
+fChiSquarePerNDFMax(0),
+fChiSquareTRDMax(0),
+fNumberOfTRDtrackletsPIDMin(0),
+fIsTRDrefit(0),
+fTPCnsigmaDeuteronMax(0),
+fTOFnsigmaDeuteronMax(0)
+{}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+AliAnalysisTaskSana::AliAnalysisTaskSana(const char *name):
+AliAnalysisTaskSE(name),
+fAODevent(NULL),
+fPIDResponse(NULL),
+fAODeventCuts(),
+fUtils(NULL),
+fOutputList(NULL),
+fQAList(NULL),
+fCentralityMin(0),
+fCentralityMax(0),
+fVertexZmin(0),
+fVertexZmax(0),
+fNumberOfVertexContributorsMin(0),
+fMagFieldSign(0),
+fDCAxyMax(0),
+fDCAzMax(0),
+fPtMin(0),
+fPtMax(0),
+fEtaMax(0),
+fNumberOfClustersITSMin(0),
+fNumberOfClustersTPCMin(0),
+fNumberOfClustersTRDMin(0),
+fNumberOfClustersTPCdEdxMin(0),
+fNumberOfCrossedRowsMin(0),
+fCrossedRowsOverFindableClsMin(0),
+fChiSquarePerNDFMax(0),
+fChiSquareTRDMax(0),
+fNumberOfTRDtrackletsPIDMin(0),
+fIsTRDrefit(0),
+fTPCnsigmaDeuteronMax(0),
+fTOFnsigmaDeuteronMax(0)
+{
+    fUtils = new AliAnalysisUtils();
+    DefineInput (0, TChain::Class());
+    DefineOutput(1, TList::Class());
+    DefineOutput(2, TList::Class());
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+AliAnalysisTaskSana::~AliAnalysisTaskSana()  {
+    
+    fOutputList->Clear();
+    delete fAODevent;
+    delete fPIDResponse;
+    delete fUtils;
+    delete fOutputList;
+    delete fQAList;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+void AliAnalysisTaskSana::UserCreateOutputObjects()  {
+    
+    fOutputList = new TList();
+    fOutputList -> SetOwner();
+    
+    fQAList = new TList();
+    fQAList -> SetOwner();
+    
+    //QA Plots of Event Selection
+    fAODeventCuts.AddQAplotsToList(fQAList);
+    
+    
+    //Number of Events
+    hEvents = new TH1F ("hEvents","",10,0,10);
+    fOutputList -> Add(hEvents);
+    
+    
+
+    hPt = new TH1F ("hPt","",1000,0,10);
+    fOutputList -> Add (hPt);
+    
+    
+    PostData(1, fOutputList);
+    PostData(2, fQAList);
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+void AliAnalysisTaskSana::UserExec(Option_t *)  {
+    
+
+    //Get Input Event
+    if ( !GetEvent ()) return;
+    
+
+    //Load PID Response
+    AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+    AliInputEventHandler *inputHandler = (AliInputEventHandler*) (mgr->GetInputEventHandler());
+    fPIDResponse = inputHandler->GetPIDResponse();
+    
+    
+    //Loop over Reconstructed Tracks
+    for (Int_t i=0 ; i<fAODevent->GetNumberOfTracks() ; i++)  {
+        
+        //Track Selection
+        AliAODTrack *track = (AliAODTrack*) fAODevent -> GetTrack(i);
+        if ( !track ) continue;
+        if ( !IsGoodQualityTrack (track)) continue;
+        
+        hPt -> Fill (track->Pt());
+    }
+    
+    
+    PostData(1, fOutputList);
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+Bool_t AliAnalysisTaskSana::GetEvent ()  {
+    
+    //Get Input Event
+    fAODevent = dynamic_cast <AliAODEvent*>(InputEvent());
+    if (!fAODevent) return false;
+    hEvents -> Fill(0.5);
+    
+    //Standard Event Cuts
+    if (!fAODeventCuts.AcceptEvent(fAODevent)) {
+        PostData(2, fQAList);
+        return false;
+    }
+    hEvents -> Fill(1.5);
+
+    //Centrality
+    AliMultSelection *multiplicitySelection = (AliMultSelection*) fAODevent->FindListObject("MultSelection");
+    if( !multiplicitySelection) return false;
+    hEvents -> Fill(2.5);
+    Double_t centrality = multiplicitySelection->GetMultiplicityPercentile("V0M");
+    
+    //Selection of Centrality Range
+    if (centrality<fCentralityMin || centrality>=fCentralityMax ) return false;
+    hEvents -> Fill(3.5);
+
+    //Primary Vertex
+    AliAODVertex *vertex = (AliAODVertex*) fAODevent->GetPrimaryVertex();
+    if ( !vertex ) return false;
+    hEvents -> Fill(4.5);
+    
+    //Primary Vertex Selection
+    if ( vertex->GetZ() < fVertexZmin ) return false;
+    if ( vertex->GetZ() > fVertexZmax ) return false;
+    hEvents -> Fill(5.5);
+    
+    if ( vertex->GetNContributors() < fNumberOfVertexContributorsMin ) return false;
+    hEvents -> Fill(6.5);
+
+    //Event Plane
+    AliEventplane *eventPlane =  fAODevent->GetEventplane();
+    if (!eventPlane) return false;
+    hEvents -> Fill(7.5);
+
+    //Magnetic Field Orientation
+    Double_t signMagneticField = fAODevent->GetMagneticField()/TMath::Abs(fAODevent->GetMagneticField());
+    if (fMagFieldSign>0 && signMagneticField<0) return false;
+    if (fMagFieldSign<0 && signMagneticField>0) return false;
+    hEvents -> Fill(8.5);
+    
+    return true;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+Bool_t AliAnalysisTaskSana::IsGoodQualityTrack (AliAODTrack *track)  {
+    
+    //Filterbit
+    if(!track->TestFilterMask(AliAODTrack::kTrkGlobalNoDCA)) return false;
+
+    
+    //Kinematic Cuts & Acceptance
+    if ( track->Pt()<fPtMin || track->Pt()>fPtMax ) return false;
+    if ( TMath::Abs(track->Eta()) > fEtaMax )       return false;
+    
+    //Track Selection Cuts
+    if ( track->GetITSNcls() < fNumberOfClustersITSMin ) return false;
+    if ( track->GetTPCNcls() < fNumberOfClustersTPCMin ) return false;
+    if ( track->GetTRDncls() < fNumberOfClustersTRDMin ) return false;
+    if ( track->GetTPCNCrossedRows() < fNumberOfCrossedRowsMin ) return false;
+    if ( static_cast<Double_t>(track->GetTPCNCrossedRows())/static_cast<Double_t>(track->GetTPCNclsF()) < fCrossedRowsOverFindableClsMin) return false;
+    if ( track->GetTPCsignalN() < fNumberOfClustersTPCdEdxMin ) return false;
+    if ( track->Chi2perNDF() > fChiSquarePerNDFMax) return false;
+    
+    
+    //DCA
+    Double_t DCAz  = GetDCAz  (track);
+    Double_t DCAxy = GetDCAxy (track);
+    
+    if (TMath::Abs(DCAz)  > fDCAzMax)  return false;
+    if (TMath::Abs(DCAxy) > fDCAxyMax) return false;
+
+    return true;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+Double_t AliAnalysisTaskSana::GetDCAxy (AliAODTrack *track)  {
+    
+    Double_t impactParameter[2];
+    Double_t covarianceMatrix[3];
+    if (!track->PropagateToDCA (fAODevent->GetPrimaryVertex(),fAODevent->GetMagneticField(),10000,impactParameter,covarianceMatrix)) return -999;
+    
+    Double_t DCAxy = impactParameter[0];
+    
+    return DCAxy;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+Double_t AliAnalysisTaskSana::GetDCAz (AliAODTrack *track)  {
+    
+    Double_t impactParameter[2];
+    Double_t covarianceMatrix[3];
+    if (!track->PropagateToDCA (fAODevent->GetPrimaryVertex(),fAODevent->GetMagneticField(),10000,impactParameter,covarianceMatrix)) return -999;
+    
+    Double_t DCAz = impactParameter[1];
+    
+    return DCAz;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+Bool_t AliAnalysisTaskSana::IsDeuteronCandidate (AliAODTrack *track)  {
+    
+    Double_t nsigmaTPC = fPIDResponse -> NumberOfSigmasTPC (track,AliPID::kDeuteron);
+    if (TMath::Abs(nsigmaTPC) > fTPCnsigmaDeuteronMax) return false;
+
+    return true;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+void AliAnalysisTaskSana::Terminate(Option_t *)  {
+    
+    fOutputList = dynamic_cast<TList*> (GetOutputData(1));
+    if (!fOutputList) return;
+}
+//_________________________________________________________________________________________________________________________________________________________________________________________________
+
---
+++ ANALYSIS_TASK_SANA/runAnalysis.C
@@ -0,0 +1,207 @@
+#define ALIPHYSICS_VER  "vAN-20190301-1"
+#define GRIDWorkingDir  "Sana_DIR"
+#define AnalysisMacro   "AnalysisSana"
+#define AnalysisTask    "AliAnalysisTaskSana"
+#define TTL             20000
+#define nRunsPerMaster  1
+
+
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadAnalysisTask (AliAnalysisManager *mgr)  {
+    
+    
+    //Analysis Cuts
+    Double_t CentralityMin =  0.0;
+    Double_t CentralityMax = 90.0;
+    Double_t VertexZmin = -10.0;
+    Double_t VertexZmax = +10.0;
+    Int_t    NumberOfVertexContributorsMin = 1;
+    Double_t MagFieldSign = 0.0;
+    Double_t PtMin =  0.5;
+    Double_t PtMax = 50.0;
+    Double_t EtaMax = 0.8;
+    Double_t DCAxyMax = 0.1;
+    Double_t DCAzMax  = 1.0;
+    Int_t    NumberOfClustersITSMin = 2;
+    Int_t    NumberOfClustersTPCMin = 80;
+    Int_t    NumberOfClustersTRDMin = 2;
+    Int_t    NumberOfClustersTPCdEdxMin = 50;
+    Int_t    NumberOfCrossedRowsMin = 70;
+    Double_t CrossedRowsOverFindableClsMin = 0.8;
+    Double_t ChiSquarePerNDFMax = 4.0;
+    Double_t ChiSquareTRDMax = 20.0;
+    Int_t    NumberOfTRDtrackletsPIDMin = 0;
+    Bool_t   IsTRDrefit = (false);
+    Double_t TPCnsigmaDeuteronMax = 3.0;
+    Double_t TOFnsigmaDeuteronMax = 3.0;
+
+    
+    //Load Analysis Tasks
+    gROOT->LoadMacro("AliAnalysisTaskSana.cxx+g");
+    
+    //Input container
+    AliAnalysisDataContainer *input = mgr -> GetCommonInputContainer();
+
+    //Analysis Task
+    AliAnalysisTaskSana *task = new AliAnalysisTaskSana  ("AliAnalysisTaskSana");
+    task -> SelectCollisionCandidates (AliVEvent::kINT7);
+    task -> AliAnalysisTaskSana::SetAnalysisCuts (CentralityMin,CentralityMax,VertexZmin,VertexZmax,NumberOfVertexContributorsMin,MagFieldSign,DCAxyMax,DCAzMax,PtMin,PtMax,EtaMax,NumberOfClustersITSMin,NumberOfClustersTPCMin,NumberOfClustersTRDMin,NumberOfClustersTPCdEdxMin,NumberOfCrossedRowsMin,CrossedRowsOverFindableClsMin,ChiSquarePerNDFMax,ChiSquareTRDMax,NumberOfTRDtrackletsPIDMin,IsTRDrefit,TPCnsigmaDeuteronMax,TOFnsigmaDeuteronMax);
+    mgr -> AddTask(task);
+    AliAnalysisDataContainer *output   = mgr -> CreateContainer("Input",TList::Class(),AliAnalysisManager::kOutputContainer,"InputFile.root");
+    AliAnalysisDataContainer *outputQA = mgr -> CreateContainer("InputQA",TList::Class(),AliAnalysisManager::kOutputContainer,"Input_QA.root");
+    mgr -> ConnectInput (task,0,input);
+    mgr -> ConnectOutput(task,1,output);
+    mgr -> ConnectOutput(task,2,outputQA);
+    
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void runAnalysis (const char *mode="full", Bool_t merge=true)  {
+    
+    //Load Libraries
+    LoadLibraries();
+    
+    //Grid Connection
+    TGrid::Connect("alien://");
+
+    //Alien Handler
+    AliAnalysisGrid *alienHandler = CreateAlienHandler (mode,merge);
+    if (!alienHandler) return;
+    
+    //Analysis Manager
+    AliAnalysisManager *mgr = new AliAnalysisManager("AnalysisManager");
+    mgr->SetGridHandler(alienHandler);
+
+    //Event Handler, PID & Centrality
+    EventHandler(mgr);
+    LoadPhysicsSelection ();
+    LoadCentrality ();
+    LoadEPSelection();
+    LoadPIDResponse();
+    
+    //Analysis Task
+    LoadAnalysisTask(mgr);
+    
+    //Start analysis
+    if (!mgr->InitAnalysis())  return;
+    mgr->PrintStatus();
+    mgr->StartAnalysis("grid");
+};
+//__________________________________________________________________________________________________________________________________________________________________________
+AliAnalysisGrid* CreateAlienHandler (const char *mode, Bool_t merge )  {
+    
+    //Alien Handler
+    AliAnalysisAlien *alien = new AliAnalysisAlien();
+    SetAdditionalLibraries (alien);
+    alien->SetOverwriteMode();
+    alien->SetCheckCopy(false);
+    alien->SetRunMode(mode);
+    alien->SetNtestFiles(10);
+    alien->SetAPIVersion("V1.1x");
+    alien->SetAliPhysicsVersion(ALIPHYSICS_VER);
+    alien->AddIncludePath("$ALICE_PHYSICS/include");
+    alien->AddIncludePath("$ALICE_ROOT/include");
+    alien->SetGridDataDir("/alice/data/2015/LHC15o");
+    alien->SetDataPattern("*/pass1_pidfix/AOD/*/AliAOD.root");
+    alien->SetRunPrefix("000");
+    SetInputRuns (alien,mode);
+    alien->SetNrunsPerMaster(nRunsPerMaster);
+    alien->SetGridWorkingDir(Form("%s",GRIDWorkingDir));
+    alien->SetGridOutputDir("OUTPUT");
+    alien->SetAnalysisSource(Form("%s.cxx",AnalysisTask));
+    alien->SetAdditionalLibs(Form("%s.cxx  %s.h",AnalysisTask,AnalysisTask));
+    alien->SetMergeViaJDL(merge);
+    alien->SetMaxMergeStages(2);
+    alien->SetAnalysisMacro(Form("%s.C",AnalysisMacro));
+    alien->SetSplitMaxInputFileNumber(50);
+    alien->SetMasterResubmitThreshold(90);
+    alien->SetTTL(TTL);
+    alien->SetExecutable(Form("%s.sh",AnalysisMacro));
+    alien->SetInputFormat("xml-single");
+    alien->SetJDLName(Form("%s.jdl",AnalysisMacro));
+    alien->SetPrice(1);
+    alien->SetSplitMode("se");
+    return alien;
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void EventHandler (AliAnalysisManager *mgr)  {
+    
+    AliAODInputHandler* aodH = new AliAODInputHandler();
+    mgr->SetInputEventHandler(aodH);
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadPhysicsSelection()  {
+    
+    gROOT->LoadMacro("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C");
+    AliPhysicsSelectionTask *physSel = AddTaskPhysicsSelection(false, true);
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadPIDResponse ()  {
+    
+    gROOT->LoadMacro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C");
+    AliAnalysisTaskPIDResponse *taskPID = AddTaskPIDResponse(false);
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadEPSelection ()  {
+
+    AliVZEROEPSelectionTask *vzeroEPSelectionTask;
+    gROOT->ProcessLine(".L $ALICE_ROOT/ANALYSIS/macros/AddTaskVZEROEPSelection.C");
+    vzeroEPSelectionTask = AddTaskVZEROEPSelection();
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadCentrality()  {
+    
+    gROOT->LoadMacro("$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/macros/AddTaskMultSelection.C");
+    AliMultSelectionTask *taskMult = AddTaskMultSelection(false);
+    taskMult->SetSelectedTriggerClass(AliVEvent::kINT7);
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void SetInputRuns (AliAnalysisAlien *alien, const char *mode)  {
+    
+    //Test Mode Option
+    if (mode=="test") {alien->AddRunNumber(245145); return;}
+    
+    //Run List
+    Int_t run[] = { 245145, 245146, 245151, 245152, 245231, 245232, 245259, 245343, 245345, 245346,
+                    245347, 245349, 245353, 245396, 245397, 245401, 245407, 245409, 245411, 245439,
+                    245441, 245446, 245450, 245452, 245454, 245496, 245497, 245501, 245504, 245505,
+                    245507, 245535, 245540, 245542, 245543, 245544, 245545, 245554 };
+    
+    Int_t nRuns = sizeof(run)/sizeof(Int_t);
+    nRuns = 1;
+    for ( Int_t iRun=0 ; iRun<nRuns ; iRun++ )
+        alien->AddRunNumber(run[iRun]);
+        
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void LoadLibraries()
+{
+    gSystem->Load("libCore.so");
+    gSystem->Load("libTree.so");
+    gSystem->Load("libGeom.so");
+    gSystem->Load("libVMC.so");
+    gSystem->Load("libPhysics.so");
+    gSystem->Load("libMinuit.so");
+    gSystem->Load("libGui.so");
+    gSystem->Load("libXMLParser.so");
+    gSystem->Load("libSTEERBase.so");
+    gSystem->Load("libESD.so");
+    gSystem->Load("libCDB.so");
+    gSystem->Load("libAOD.so");
+    gSystem->Load("libANALYSIS.so");
+    gSystem->Load("libANALYSISalice.so");
+    
+    gROOT->ProcessLine(".include $ALICE_PHYSICS/include");
+    gROOT->ProcessLine(".include $ALICE_ROOT/ANALYSIS/");
+    gROOT->ProcessLine(".include $ALICE_ROOT/include");
+    gROOT->ProcessLine(".include $ROOTSYS/include");
+    
+    gSystem->SetIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_PHYSICS -I$ALICE_PHYSICS/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/STEER/STEER -I$ALICE_ROOT/STEER/STEERBase -I$ALICE_ROOT/STEER/ESD -I$ALICE_ROOT/STEER/AOD -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS  -I$ALICE_PHYSICS/OADB -I$ALICE_PHYSICS/PWGHF -I$ALICE_PHYSICS/PWGHF/base -I$ALICE_PHYSICS/PWGHF/vertexingHF -I$ALICE_PHYSICS/PWG/FLOW/Base -I$ALICE_PHYSICS/PWG/FLOW/Tasks -I$ALICE_PHYSICS/JETAN -I$ALICE_PHYSICS/PWG/Tools -g");
+}
+//__________________________________________________________________________________________________________________________________________________________________________
+void SetAdditionalLibraries (AliAnalysisAlien *alien)  {
+    
+    alien->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_PHYSICS -I$ALICE_PHYSICS/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/STEER/STEER -I$ALICE_ROOT/STEER/STEERBase -I$ALICE_ROOT/STEER/ESD -I$ALICE_ROOT/STEER/AOD -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS  -I$ALICE_PHYSICS/OADB -I$ALICE_PHYSICS/PWGHF -I$ALICE_PHYSICS/PWGHF/base -I$ALICE_PHYSICS/PWGHF/vertexingHF -I$ALICE_PHYSICS/PWG/FLOW/Base -I$ALICE_PHYSICS/PWG/FLOW/Tasks -I$ALICE_PHYSICS/JETAN -I$ALICE_PHYSICS/PWG/Tools -g");
+    
+    alien->SetAdditionalLibs("AliAnalysisTaskFlowITSTPCTOFQCSP_Template.cxx AliAnalysisTaskFlowITSTPCTOFQCSP_Template.h libPWGHFhfe.so libCDB.so libSTEER.so  libCORRFW.so libPWGflowBase.so libPWGflowTasks.so libGui.so libProof.so libMinuit.so libXMLParser.so libRAWDatabase.so libRAWDatarec.so libCDB.so libSTEERBase.so libSTEER.so libTPCbase.so libTOFbase.so libTOFrec.so libTRDbase.so libVZERObase.so libVZEROrec.so libT0base.so libT0rec.so ");
+}
+//__________________________________________________________________________________________________________________________________________________________________________
